af1a9ba1ebd418d5fb2e5c773692ec4637e0aea2
An Le Dai <ledaian22@gmail.com>
2025-07-14 01:54:28 +0700

    first commit

---
+++ components/navbar.tsx
@@ -0,0 +1,65 @@
+import {
+  Navbar as HeroUINavbar,
+  NavbarContent,
+  NavbarBrand,
+  NavbarItem,
+} from "@heroui/navbar";
+import { Button } from "@heroui/button";
+import { Link } from "@heroui/link";
+import NextLink from "next/link";
+
+import { siteConfig } from "@/config/site";
+import { ThemeSwitch } from "@/components/theme-switch";
+import { IconHeartFilled } from "@tabler/icons-react";
+
+export const Navbar = () => {
+  return (
+    <HeroUINavbar maxWidth="xl" position="sticky">
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <p className="font-bold text-inherit">AIO APPS</p>
+          </NextLink>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="end"
+      >
+        <NavbarItem className="hidden sm:flex gap-2">
+          <ThemeSwitch />
+        </NavbarItem>
+
+        <NavbarItem>
+          <Button
+            isExternal
+            as={Link}
+            className="text-sm font-normal text-default-600 bg-default-100"
+            href={siteConfig.links.sponsor}
+            variant="flat"
+            startContent={<IconHeartFilled size={22} color="red" />}
+          >
+            Source Code
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+        <ThemeSwitch />
+        <NavbarItem>
+          <Button
+            isExternal
+            as={Link}
+            className="text-sm font-normal text-default-600 bg-default-100"
+            href={siteConfig.links.sponsor}
+            variant="flat"
+            startContent={<IconHeartFilled size={22} color="red" />}
+          >
+            Source Code
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </HeroUINavbar>
+  );
+};
---
+++ config/site.ts
@@ -0,0 +1,10 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js + HeroUI",
+  description: "Make beautiful websites regardless of your design experience.",
+
+  links: {
+    sponsor: "https://github.com/heroui-inc/heroui",
+  },
+};
